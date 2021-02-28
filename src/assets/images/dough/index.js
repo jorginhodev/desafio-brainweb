4ead5eef9681b2ff1a69a76f10f5eb72c465b1b2
Jorge Bierhals Ramos <jorginhodeveloper@gmail.com>
2021-02-28 20:39:33 -0300

    feat(static): add pizza dough images

---
+++ src/assets/images/dough/index.js
@@ -0,0 +1,5 @@
+import Napolitana from "./pizza-napolitana.jpg";
+import Siciliana from "./pizza-siciliana.jpg";
+import NovaIorquina from "./pizza-nova-iorquina.jpg";
+
+export default { Napolitana, Siciliana, NovaIorquina };
