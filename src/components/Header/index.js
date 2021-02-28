4cf02f66544b7052daf87bd3d41bf7b3eb380361
Jorge Bierhals Ramos <jorge.ramos@atlastechnol.com>
2021-02-28 02:55:41 -0300

    feat(ui) create Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import * as S from "./styles";
+
+const Header = () => {
+    return (
+        <S.Wrapper>
+            <h1>Brain Pizzaria</h1>
+        </S.Wrapper>
+    );
+};
+
+export default Header;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.header`
+    background-color: #2e3236;
+    height: 70px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    h1 {
+        text-transform: uppercase;
+        font-size: 28px;
+    }
+`;
