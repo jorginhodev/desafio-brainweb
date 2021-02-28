8f1caeb1d539a37df73b0ad70117ded452d647e9
Jorge Bierhals Ramos <jorginhodeveloper@gmail.com>
2021-02-28 20:43:00 -0300

    feat(routes): add componentes to router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
 import { Route, Switch } from "react-router-dom";
 
 import Home from "../pages/Home";
+import Mount from "../pages/Mount";
+import Recommendation from "../pages/Recommendation";
 
 const Routes = () => (
     <Switch>
         <Route path="/" component={Home} exact />
+        <Route path="/mount" component={Mount} />
+        <Route path="/recommendation" component={Recommendation} />
     </Switch>
 );
 
