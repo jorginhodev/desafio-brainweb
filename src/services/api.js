30bec321ebacf2c367b991bdb962f3da80e08532
Jorge Bierhals Ramos <jorginhodeveloper@gmail.com>
2021-02-28 20:42:23 -0300

    feat: add api

---
+++ src/services/api.js
@@ -0,0 +1,89 @@
+import DoughImages from "../assets/images/dough";
+import SizeImages from "../assets/images/size";
+
+export const doughList = [
+    {
+        id: "1",
+        title: "Napolitana",
+        description:
+            "O modo napolitano de preparo é considerado um Patrimônio Cultural Imaterial da Humanidade pela Unesco.",
+        price: 10.0,
+        image: DoughImages.Napolitana,
+    },
+    {
+        id: "2",
+        title: "Nova-iorquina",
+        description:
+            "Tem uma massa mais grossa e é levemente crocante por fora, dessa forma são consumidas sem talheres.",
+        price: 12.0,
+        image: DoughImages.NovaIorquina,
+    },
+    {
+        id: "3",
+        title: "Siciliana",
+        description:
+            "A pizza siciliana é conhecida por sua massa grossa em forma de retângulo, geralmente com mais de um centímetro de espessura.",
+        price: 15.0,
+        image: DoughImages.Siciliana,
+    },
+];
+
+export const sizeList = [
+    {
+        id: "1",
+        title: "Pizza Pequena - 4 Pedaços",
+        price: 8.0,
+        image: SizeImages.FourPieces,
+    },
+    {
+        id: "2",
+        title: "Pizza Média - 8 Pedaços",
+        price: 10.0,
+        image: SizeImages.EightPieces,
+    },
+    {
+        id: "3",
+        title: "Pizza Grande - 12 Pedaços",
+        price: 14.0,
+        image: SizeImages.TwelvePieces,
+    },
+];
+
+export const fillingList = [
+    {
+        id: "1",
+        title: "Calabresa",
+        price: 10.0,
+        image: DoughImages.Napolitana,
+    },
+    {
+        id: "2",
+        title: "Confete",
+        price: 10.0,
+        image: DoughImages.NovaIorquina,
+    },
+    {
+        id: "3",
+        title: "Frango c/ catupiry",
+        price: 12.0,
+        image: DoughImages.Siciliana,
+    },
+    {
+        id: "4",
+        title: "Marguerita",
+        price: 12.0,
+        image: DoughImages.Napolitana,
+    },
+    {
+        id: "5",
+        title: "Muçarela",
+        price: 10.0,
+        image: DoughImages.NovaIorquina,
+    },
+    {
+        id: "6",
+        title: "Portuguesa",
+        price: 11.0,
+        image: DoughImages.Siciliana,
+    },
+];
