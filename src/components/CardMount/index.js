09a3714af052c23e4fd79c4dccb5478f2a2a1df2
Jorge Bierhals Ramos <jorge.ramos@atlastechnol.com>
2021-02-28 02:56:45 -0300

    feat(ui): create Card Mount component

---
+++ src/components/CardMount/index.js
@@ -0,0 +1,37 @@
+import Button from "../Button";
+import PizzaImg from "../../assets/images/pizza.svg";
+import CheckIcon from "../../assets/images/check.svg";
+import ArrowRightIcon from "../../assets/images/arrow-right.svg";
+import * as S from "./styles";
+
+const CardMount = () => {
+    return (
+        <S.Wrapper>
+            <h1>Monte sua Pizza</h1>
+
+            <img src={PizzaImg} alt="Ilustração de uma pizza" />
+
+            <S.Benefits>
+                <span>
+                    <img src={CheckIcon} alt="Ícone de checkbox" />
+                    Escolha o tipo de massa
+                </span>
+
+                <span>
+                    <img src={CheckIcon} alt="Ícone de checkbox" />
+                    Escolha o recheio
+                </span>
+            </S.Benefits>
+
+            <Button to="/mount">
+                <span>Monte sua Pizza</span>
+                <img
+                    src={ArrowRightIcon}
+                    alt="Ícone de uma seta para a direita"
+                />
+            </Button>
+        </S.Wrapper>
+    );
+};
+
+export default CardMount;
---
+++ src/components/CardMount/styles.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
+    background-color: #2e3236;
+    width: 420px;
+    height: 450px;
+    background: #2e3236;
+    box-shadow: 0px 0px 60px rgba(0, 0, 0, 0.05);
+    border-radius: 5px;
+    padding: 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+
+    h1 {
+        text-transform: uppercase;
+        font-size: 20px;
+    }
+`;
+
+export const Benefits = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    span {
+        display: flex;
+
+        img {
+            margin-right: 15px;
+            width: 20px;
+        }
+    }
+`;
