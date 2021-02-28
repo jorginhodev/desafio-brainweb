9462d5a2acb8e041be25674e30127ad2a081ad1b
Jorge Bierhals Ramos <jorginhodeveloper@gmail.com>
2021-02-28 20:39:56 -0300

    feat(static): add pizza size images

---
+++ src/assets/images/size/index.js
@@ -0,0 +1,5 @@
+import FourPieces from "./pizza-4.svg";
+import EightPieces from "./pizza-8.svg";
+import TwelvePieces from "../pizza.svg";
+
+export default { FourPieces, EightPieces, TwelvePieces };
