0ef21354ac7844e6df7e3e085e3594fd1b3d348d
Jorge Bierhals Ramos <jorge.ramos@atlastechnol.com>
2021-02-28 02:57:13 -0300

    feat(ui): create Card Recommendation component

---
+++ src/components/CardRecommendation/index.js
@@ -0,0 +1,33 @@
+import Button from "../Button";
+import PizzaImg from "../../assets/images/pizza.svg";
+import ArrowRightIcon from "../../assets/images/arrow-right.svg";
+import * as S from "./styles";
+
+const CardRecommendation = () => {
+    return (
+        <S.Wrapper>
+            <h1>
+                Escolha a recomendação do dia e ganhe{" "}
+                <strong>100 pontos</strong>!
+            </h1>
+
+            <img src={PizzaImg} alt="Ilustração de uma pizza" />
+
+            <S.Information>
+                <span>Sabor da Pizza</span>
+
+                <span>Valor: R$ 99,00</span>
+            </S.Information>
+
+            <Button to="/recommendation">
+                <span>Escolher a Recomendação</span>
+                <img
+                    src={ArrowRightIcon}
+                    alt="Ícone de uma seta para a direita"
+                />
+            </Button>
+        </S.Wrapper>
+    );
+};
+
+export default CardRecommendation;
