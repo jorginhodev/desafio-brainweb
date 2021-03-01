585a5fcd6bcb4953ac12034a7385a318797e0ca3
Jorge Bierhals Ramos <jorginhodeveloper@gmail.com>
2021-03-01 04:30:56 -0300

    refactor: add prop onclick

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,9 @@
 import PropTypes from "prop-types";
 import * as S from "./styles";
 
-const Button = ({ children, to, bgcolor }) => {
+const Button = ({ children, to, bgcolor, onClick }) => {
     return (
-        <S.Button to={to} color={bgcolor}>
+        <S.Button to={to} color={bgcolor} onClick={onClick}>
             {children}
         </S.Button>
     );
@@ -13,6 +13,7 @@ Button.propTypes = {
     children: PropTypes.node.isRequired,
     to: PropTypes.string.isRequired,
     bgcolor: PropTypes.string,
+    onClick: PropTypes.func,
 };
 
 export default Button;
