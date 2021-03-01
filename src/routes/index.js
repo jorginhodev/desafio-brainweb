a879ead840765112c07bb49053000d405e397a74
Jorge Bierhals Ramos <jorginhodeveloper@gmail.com>
2021-03-01 03:00:12 -0300

    refactor: remove page Recommendation

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,12 @@ import { Route, Switch } from "react-router-dom";
 
 import Home from "../pages/Home";
 import Mount from "../pages/Mount";
-import Recommendation from "../pages/Recommendation";
 import Checkout from "../pages/Checkout";
 
 const Routes = () => (
     <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/mount" component={Mount} />
-        <Route path="/recommendation" component={Recommendation} />
         <Route path="/checkout" component={Checkout} />
     </Switch>
 );
